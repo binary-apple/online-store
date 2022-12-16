56b133293def210e0bbcfd2130e56dbf39521008
Alexandr Eremeev <eremeev.a.e@mail.ru>
2022-12-16 18:41:03 +0700

    fix: clear main page

--- src/components/pages/mainPage/MainPage.ts
+++ src/components/pages/mainPage/MainPage.ts
@@ -1,34 +1,15 @@
-import { IInstanceContext } from '../../../utils/context/types/IContext';
 import { IProductsAPI } from '../../../api/types/IProducts';
 
 class MainPage {
     products = {} as IProductsAPI;
 
-    async init(context: IInstanceContext) {
-        this.products = await context.$api?.getProducts(null);
-
-        const cards = this.products?.products.map((item) => `<div>${item.title}</div>`);
-
-        return await this.render(cards);
+    async init() {
+        return await this.render();
     }
 
-    render(cards: Array<string>) {
+    render() {
         return `
-            <ul>
-                ${cards?.join('')}
-                <li>
-                    <a href="/cart">cart</a>
-                </li>
-                <li>
-                    <a href="/product/1">product</a>
-                </li>
-                <li>
-                    <a href="/product/2">product2</a>
-                </li>
-                <li>
-                    <a href="/product/3">product3</a>
-                </li>
-            </ul>
+            <ul></ul>
         `;
     }
 }
