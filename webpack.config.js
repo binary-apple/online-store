720f8af4573037e48903472c55058a0275fddd1a
Alexandr Eremeev <eremeev.a.e@mail.ru>
2022-12-22 02:00:25 +0700

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const baseConfig = {
     entry: {
@@ -16,9 +15,6 @@ const baseConfig = {
     },
     mode: 'development',
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/styles.css',
-        }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             publicPath: './',
@@ -48,19 +44,7 @@ const baseConfig = {
             },
             {
                 test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: ['postcss-preset-env'],
-                            },
-                        },
-                    },
-                    'sass-loader',
-                ],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
         ],
     },
