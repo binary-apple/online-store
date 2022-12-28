6052110416f226918a8624fa8cf5d79d04de277e
Anna Perevedentseva <anna.pereved@gmail.com>
2022-12-29 00:30:41 +0100

    feat: implement error page

---
+++ src/components/error-content.ts
@@ -0,0 +1,21 @@
+import { Component } from "./types/component";
+
+export class Error extends Component{
+    constructor() {
+        super({containerTag: 'div', className: 'w-100 d-flex align-items-center'.split(' ')});
+    }
+
+    protected template(): DocumentFragment {
+        const temp = document.createElement('template');
+        temp.innerHTML = `
+        <div class="error container w-75 h-50 rounded d-flex align-items-center justify-content-center">
+            <div class="d-flex flex-column align-items-center py-5">
+                <div class="e404">404</div>
+                <div class="e404-text">Oooops... Page not found</div>
+                <button onclick="document.location='/'">Go home</button>
+            </div>
+        </div>
+        `;
+        return temp.content;
+    }
+}
