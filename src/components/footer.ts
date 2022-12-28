fabfc754097aa77ccdf431f478815ba31d24c85f
Anna Perevedentseva <anna.pereved@gmail.com>
2022-12-28 16:33:58 +0100

    fix: remove blank target for links

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -15,18 +15,18 @@ export class Footer extends Component{
                         <div class="github-logo"></div>
                         <div class="github-text text-nowrap">2022 by </div>
                         <div class="github-text text-nowrap lh-base">
-                            <a href="https://github.com/nepo92" target="_blank" class="d-block">nepo92</a>
-                            <a href="https://github.com/binary-apple" target="_blank" class="d-block">binary-apple</a>
+                            <a href="https://github.com/nepo92" class="d-block">nepo92</a>
+                            <a href="https://github.com/binary-apple" class="d-block">binary-apple</a>
                         </div>
                     </div>
                 </div>
                 <div class="col flex-grow-0">
-                    <a href="https://rs.school/js/" target="_blank" class="rsschool d-block"></a>
+                    <a href="https://rs.school/js/" class="rsschool d-block"></a>
                 </div>
                 <div class="col flex-grow-0">
                     <div>
                         <div class="designed text-nowrap lh-sm">Designed by</div>
-                        <a href="https://zasovskiy.ru/dizajn-prilozheniya-severyanochka/" target="_blank" class="zasovskiy d-block"></a>
+                        <a href="https://zasovskiy.ru/dizajn-prilozheniya-severyanochka/" class="zasovskiy d-block"></a>
                     </div>
                 </div>
             </div>
