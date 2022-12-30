0a75103813db95022a3921f6a6388ca1348fd534
Alexandr Eremeev <eremeev.a.e@mail.ru>
2022-12-30 22:29:54 +0700

    fix: remove request from main controller

--- src/controller/main/main-controller.ts
+++ src/controller/main/main-controller.ts
@@ -1,8 +1,5 @@
 import Controller from '../controller';
 import Router from 'vanilla-router';
-import Request from '../../api/request';
-
-const request = new Request();
 
 class MainController extends Controller {
     constructor(router: Router) {
