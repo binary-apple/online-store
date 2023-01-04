b1ee5d6dbe9627456595912096ac5b233ec25aad
Anna Perevedentseva <anna.pereved@gmail.com>
2023-01-04 15:42:44 +0100

    fix: header's handlers

--- src/controller/main/main-controller.ts
+++ src/controller/main/main-controller.ts
@@ -1,5 +1,8 @@
 import Controller from '../controller';
 import Router from 'vanilla-router';
+import Routers from '../../router/routers';
+import { Cart } from '../../model/cart';
+import { MainView } from '../../view/main-view';
 
 class MainController extends Controller {
     constructor(router: Router) {
@@ -9,6 +12,23 @@ class MainController extends Controller {
     async init() {
         /* eslint-disable-next-line */
         console.log('products');
+
+        let cart = new Cart();
+
+        const view = new MainView(cart);
+        view.init(this.root);
+
+        view.handleClickToCartIcon(this.handleClickToCartIcon.bind(this));
+        view.handleClickToLogoIcon(this.handleClickToLogoIcon.bind(this));
+
+    }
+
+    private handleClickToCartIcon(e: Event){
+        this.router.navigateTo(Routers.CART);
+    }
+
+    private handleClickToLogoIcon(e: Event){
+        this.router.navigateTo(Routers.MAIN);
     }
 }
 
